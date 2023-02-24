5c592e678f360c81edf2506b17216615c76542bb
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-02-23 20:26:33 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,12 +1,8 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
-  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '27017') +
-    '/mernproject',
+  jwtSecret: process.env.JWT_SECRET || "n2r5u8x/TjWnZr4ubQeThWmZG+KbPeSh%D*G-KaPx!z%C*F-4t7w!z$CmYq3t6w9SgVkYp3saNdRgUkX",
+  mongoUri: process.env.MONGO_URI || "mongodb+srv://ydballer810:2kvZsZfmnvHM2zQ@cluster0.xvvrjqc.mongodb.net/?retryWrites=true&w=majority",
   stripe_connect_test_client_id: 'YOUR_stripe_connect_test_client',
   stripe_test_secret_key: 'YOUR_stripe_test_secret_key',
   stripe_test_api_key: 'YOUR_stripe_test_api_key' 
